5ce78e6272a2f1645fab6cb48590712602d4b62a
VAZ <vazxxx@mail.ru>
2022-12-19 17:35:07 +0300

    что-то

---
+++ Задача 34/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите размер массива");
+int arrayLength = int.Parse(Console.ReadLine()!);
+//Console.WriteLine("Введите минимальное значение в массиве");
+//int mivValue = int.Parse(Console.ReadLine()!);
+//Console.WriteLine("Введите максимальное значение в массиве");
+//int maxValue = int.Parse(Console.ReadLine()!);
+
+int [] array = CreateArray(arrayLength, 100, 999);
+PrintArray(array);
+int oddCount = CountOdd(array);
+Console.WriteLine(oddCount);
+
+
+int [] CreateArray(int length, int minValue, int maxValue)
+
+{
+    int[] array = new int [length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array); //Вывод массива на экран
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int CountOdd(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 1)
+        counter++;
+    }
+    return counter;
+}
